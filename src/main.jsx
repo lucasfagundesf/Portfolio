c863e767fa7201ba8578c878b37e02180043888c
Lucas Fagundes <lucas_fagundesfranco@hotmail.com>
2025-05-05 09:28:34 -0300

    :wrench: chore: Configurando os routs

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-import GlobalStyles from './styles/global';
-import theme from './styles/theme';
-import { Home } from './pages/Home';
-import { Sobre } from './pages/Sobre';
-import { Projetos } from './pages/Projetos';
-import { Contatos } from './pages/Contatos';
+import GlobalStyles from "./styles/global";
+import theme from "./styles/theme";
+import { Home } from "./pages/Home";
+import { Sobre } from "./pages/Sobre";
+import { Projetos } from "./pages/Projetos";
+import { Contatos } from "./pages/Contatos";
 
-const router = createBrowserRouter ([
+const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home/>
+    path: "/Portfolio",
+    element: <Home />,
   },
   {
     path: "/projetos",
-    element: <Projetos/>
+    element: <Projetos />,
   },
   {
     path: "/sobre",
-    element: <Sobre/>
+    element: <Sobre />,
   },
   {
     path: "/contatos",
-    element: <Contatos/>
-  }
+    element: <Contatos />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
-)
+);
